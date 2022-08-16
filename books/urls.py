7c7bf24a853017ef11d54f4b57724f344ff77f3b
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-08-16 15:54:02 +0530

    [pavan]:Refactored urls in books/urls.py file

--- books/urls.py
+++ books/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
-from books.views import GetBookAPI, AddBookAPI, DeleteBookAPI, UpdateBookAPI
+from books.views import (
+    GetBookAPI,
+    AddBookAPI,
+    DeleteBookAPI,
+    UpdateBookAPI
+)
 
 urlpatterns = [
     path('api/create/', AddBookAPI.as_view(), name='Add_Book'),
     path('api/retrieve/', GetBookAPI.as_view(), name='Get_Book'),
-    # path('api/retrieve1/', GetBookAPI1.as_view(), name='Get_Book'),
     path('api/update/<int:pk>/', UpdateBookAPI.as_view(), name='Update_Book'),
     path('api/delete/<int:pk>/', DeleteBookAPI.as_view(), name='Del_Book'),
-
 ]
