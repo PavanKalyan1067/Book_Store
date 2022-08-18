1f76d61ae8e6ff3d0ba12dee51f1d3a8d3f86d92
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-08-18 20:19:48 +0530

    [pavan]:Refactored forms.py file

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,27 +1,20 @@
-from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 
+# Create your forms here.
 from accounts.models import User
 
 
-class OrderForm(ModelForm):
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ("first_name", "last_name", "username", "email", "password1", "password2")
 
-
-class CreateUserForm(UserCreationForm):
-    # first_name = forms.CharField(max_length=255)
-    # last_name = forms.CharField(max_length=255)
-    # username = forms.CharField(max_length=255)
-    # email = forms.EmailField(max_length=255)
-    # password = forms.CharField(max_length=250)
-    # confirm_password = forms.CharField(max_length=250)
-    #
-    # class Meta:
-    #     model = User
-    #     fields = ['first_name', 'last_name', 'username', 'email', 'password', 'confirm_password']
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2")
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
