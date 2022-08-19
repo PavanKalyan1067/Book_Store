92757f70526c178e33befa5fe6b67e12a6965867
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-08-19 21:59:15 +0530

    [pavan]:Added choice fields in order models

--- orders/models.py
+++ orders/models.py
@@ -2,14 +2,19 @@ from django.conf import settings
 from django.db import models
 
 from books.models import Book
-from carts.models import Cart
+
+
+class Status(models.TextChoices):
+    CA = 'CART'
+    OR = 'ORDER'
+    WL = 'WISHLIST'
 
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.PROTECT)
     book_quantity = models.IntegerField(null=False)
     address = models.TextField(max_length=300, null=False)
     total_price = models.IntegerField(null=False)
     date_ordered = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=Status.choices, default=Status.CA, max_length=10)
