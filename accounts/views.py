0e83c902da996e2eb8ec1a7049989b516b621a0a
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-08-19 07:16:56 +0530

    [pavan]:Deleted extra added files

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 import jwt
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
-
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.conf import settings
@@ -64,7 +64,6 @@ def registerPage(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            print('abcd')
             messages.success(request, "Registration successful.")
             return redirect('profile')
         messages.error(request, "Unsuccessful registration. Invalid information.")
