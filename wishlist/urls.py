f96df0ccd56d06fa12cd59d4311f32b489b58d4f
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-08-19 23:59:09 +0530

    [pavan]:Refactored urls

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from wishlist.views import AddToWishlistAPI, GetWishlistAPIView, DeleteWishlistAPI
 
 urlpatterns = [
-    path('api/add/', AddToWishlistAPI.as_view(), name="adding_to_wishlist"),
-    path('api/retrieve/', GetWishlistAPIView.as_view(), name="view_wishlist"),
+    path('api/add/', AddToWishlistAPI.as_view(), name="add_to_wishlist"),
+    path('api/retrieve/', GetWishlistAPIView.as_view(), name="get_wishlist"),
     path('api/delete/<int:pk>/', DeleteWishlistAPI.as_view(), name='delete_cart'),
 
 ]
