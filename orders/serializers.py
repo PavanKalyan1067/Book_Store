73f452b201ddcdaad796b44783e509184657ee27
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-08-19 22:03:12 +0530

    [pavan]:Refactored models in the serializers.py

--- orders/serializers.py
+++ orders/serializers.py
@@ -30,7 +30,6 @@ class OrderSerializer(serializers.ModelSerializer):
 class GetOrderSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField()
     book_id = serializers.IntegerField()
-    cart_id = serializers.IntegerField()
     book_quantity = serializers.IntegerField()
     total_price = serializers.FloatField()
     address = serializers.CharField()
@@ -40,7 +39,6 @@ class GetOrderSerializer(serializers.ModelSerializer):
         fields = [
             'user_id',
             'book_id',
-            'cart_id',
             'book_quantity',
             'total_price',
             'address'
