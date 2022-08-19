4df87efb8ddf7aab42c0655580eb83e68e87e24f
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-08-19 22:04:37 +0530

    [pavan]:Added delete url in the urls.py

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from wishlist.views import AddToWishlistAPI, GetWishlistAPIView
+
+from wishlist.views import AddToWishlistAPI, GetWishlistAPIView, DeleteWishlistAPI
 
 urlpatterns = [
     path('api/add/', AddToWishlistAPI.as_view(), name="adding_to_wishlist"),
     path('api/retrieve/', GetWishlistAPIView.as_view(), name="view_wishlist"),
+    path('api/delete/<int:pk>/', DeleteWishlistAPI.as_view(), name='delete_cart'),
+
 ]
