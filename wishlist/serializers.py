e7c6ea2b88b553e17ab865cb4791001160887b17
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-08-19 22:03:23 +0530

    [pavan]:Refactored models in the serializers.py

--- wishlist/serializers.py
+++ wishlist/serializers.py
@@ -1,17 +1,30 @@
 from rest_framework import serializers
-from wishlist.models import Wishlist
+
+from orders.models import Order
 
 
 class WishlistSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Wishlist
+        model = Order
         fields = [
             'book',
-            'user'
+            'book_quantity',
+            'user',
         ]
+
+    def create(self, validated_data):
+        book = validated_data.get('book')
+        book_quantity = validated_data.get('book_quantity')
+        if book.book_quantity < book_quantity:
+            raise Exception('book out of stock')
+        book.book_quantity -= book_quantity
+        book.save()
+        total_price = book.price * book_quantity
+        validated_data.update({'total_price': total_price})
+        return self.Meta.model.objects.create(**validated_data)
 
 
 class GetWishlistSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Wishlist
+        model = Order
         fields = '__all__'
