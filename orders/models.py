076840c504b1c66247d6696486c15831ef2b932e
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-08-22 22:20:48 +0530

    [pavan]:Removed Address field from order/order model

--- orders/models.py
+++ orders/models.py
@@ -14,7 +14,6 @@ class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.PROTECT)
     book_quantity = models.IntegerField(null=False)
-    address = models.TextField(max_length=300, null=False)
     total_price = models.IntegerField(null=False)
     date_ordered = models.DateTimeField(auto_now_add=True)
     status = models.CharField(choices=Status.choices, default=Status.CA, max_length=10)
