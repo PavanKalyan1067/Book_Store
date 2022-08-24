49b81cbac3e3c615a181a96146d309f3b4b8498f
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-08-24 08:46:08 +0530

    [pavan]:Refactored signal.py

--- carts/serializers.py
+++ carts/serializers.py
@@ -14,16 +14,16 @@ class AddCartSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['id', 'status']
 
-    def create(self, validated_data):
-        book = validated_data.get('book')
-        book_quantity = validated_data.get('book_quantity')
-        if book.book_quantity < book_quantity:
-            raise Exception('book out of stock')
-        book.book_quantity -= book_quantity
-        book.save()
-        total_price = book.price * book_quantity
-        validated_data.update({'total_price': total_price})
-        return self.Meta.model.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     book = validated_data.get('book')
+    #     book_quantity = validated_data.get('book_quantity')
+    #     # if book.book_quantity < book_quantity:
+    #     #     raise Exception('book out of stock')
+    #     # book.book_quantity -= book_quantity
+    #     # book.save()
+    #     # total_price = book.price * book_quantity
+    #     # validated_data.update({'total_price': total_price})
+    #     return self.Meta.model.objects.create(**validated_data)
 
 
 class GetAllCartSerializer(serializers.ModelSerializer):
--- orders/signal.py
+++ orders/signal.py
@@ -1,35 +1,13 @@
-from django.db.models.signals import post_save, pre_save, post_delete, pre_delete
-from django.core.signals import request_finished
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
 from orders.models import Order
-from django.dispatch import receiver, Signal
 
 
 @receiver(pre_save, sender=Order)
 def create_order(sender, instance, **kwargs):
-    print('order created')
-
-
-@receiver(post_save, sender=Order)
-def update_order(sender, instance, **kwargs):
-    print('order updated')
-
-
-@receiver(post_delete, sender=Order)
-def delete_order(sender, instance, **kwargs):
-    print('order deleted')
-
-
-@receiver(request_finished)
-def my_callback(sender, **kwargs):
-    print("Request finished!")
-
-
-abd = Signal()
-abd.send(sender=Order, abd1='hey')
-
-
-@receiver(abd)
-def my_task_done(sender, **kwargs):
-    print(kwargs)
-
-
+    if instance.book.book_quantity < instance.book_quantity:
+        raise Exception('book out of stock')
+    instance.book.book_quantity -= instance.book_quantity
+    instance.book.save()
+    instance.total_price = instance.book.price * instance.book_quantity
