ce1684486666b96b5c445f9c7d2db2c97f2d1b92
PavanKalyan1067 <pavankalyanyadav321@gmail.com>
2022-08-25 19:39:26 +0530

    [pavan]:Refactored utils file from accounts app

--- accounts/utils.py
+++ accounts/utils.py
@@ -47,5 +47,4 @@ class Util:
         email = EmailMessage(
             subject=data['email_subject'], body=data['email_body'], from_email=data['from_email'],
             to=[data['to_email']])
-        # server.quit()
         EmailThread(email).start()
